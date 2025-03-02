ab07ab22dc82493b5088ef4394f560547de65b03
AggelosKoralis <angelos.koralis@gmail.com>
2025-03-02 16:06:40 +0200

    added more functions and default compare/free functions to the list struct

--- list/list.c
+++ list/list.c
@@ -12,11 +12,22 @@ struct node {
 struct list {
     Node nodes;
     uint64_t size;
+    compare_func compare;
+    free_node_func free_node;
 };
 
 void boom(char *msg) {
     fprintf(stderr, msg);
     exit(1);
+}
+
+
+void list_default_free_node(Node node) {
+    free(node);
+}
+
+int list_default_compare(datatype a, datatype b) {
+    return a - b;
 }
 
 
@@ -32,7 +43,8 @@ Node list_make_node(datatype data, Node next) {
 }
 
 
-List list_init(void) {
+List list_init(compare_func compare, free_node_func free_node) {
+    
     List new_list = malloc(sizeof(struct list));
     if (!new_list) 
         boom("Failed to allocte memery for the new list.\n");
@@ -40,12 +52,19 @@ List list_init(void) {
     new_list->size = 0;
     new_list->nodes = NULL;
 
+
+    if (!compare) compare = list_default_compare;
+    if (!free_node) free_node = list_default_free_node;
+
+    new_list->compare = list_default_compare;
+    new_list->free_node = list_default_free_node;
+
     return new_list;
 }
 
 int list_insert_head(List list, datatype data) {
     if (!list) {
-        list = list_init();
+        list = list_init(list_default_compare, list_default_free_node);
         if (list_insert_head(list, data) == 0) return 0;
     }
 
@@ -59,22 +78,24 @@ int list_insert_head(List list, datatype data) {
 
 
 Node list_get_tail(List list) {
-    Node head = list->nodes;
+    Node node = list->nodes;
 
-    while (head->next) 
-        head = head->next;
+    while (node->next) 
+        node = node->next;
 
-    return head;
+    return node;
 }
 
 
 int list_insert_tail(List list, datatype data) {
     if (!list) {
-        list = list_init();
+        list = list_init(list_default_compare, list_default_free_node);
         // if there are 0 or 1 nodes, head = tail
         if (list_insert_head(list, data) == 0) return 0;
     }
     
+    if (!list->nodes) return 1;
+
     Node previous_tail = list_get_tail(list);
 
     previous_tail->next = list_make_node(data, NULL);
@@ -90,7 +111,7 @@ int list_remove_head(List list) {
     Node previous_head = list->nodes->next;
     Node new_head = previous_head->next;
 
-    list_node_free(previous_head);
+    list_node_free(previous_head, list->free_node);
 
     list->nodes = new_head;
     list->size--;
@@ -103,10 +124,73 @@ int list_remove_tail(List list) {
     if (!list) return 1;
     
     Node previous_tail = list_get_tail(list);
+    Node new_tail = list_parent(list, previous_tail);
 
-
-
+    list_node_free(previous_tail, list->free_node);
+    new_tail->next = NULL;
     list->size--;
 
     return 0;
+}
+
+
+Node list_parent(List list, Node target) {
+    if (!list) return NULL;
+
+    Node parent = list->nodes;
+    if (!parent) return NULL;
+    
+    Node child = parent->next;
+
+    while (child) {
+        if (child == target) return parent;
+        parent = child;
+        child = child->next;
+    }
+
+    return NULL;
+}
+
+
+Node list_search(List list, datatype target) {
+    if (!list) return NULL;
+    if (!list->nodes) return NULL;
+
+    Node node = list->nodes;
+
+    while (node) {
+        if (list->compare(node->data, target) == 0)
+            return node;
+
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+
+uint64_t list_size(List list) {
+    return list == NULL ? 0 : list->size;
+}
+
+
+void list_node_free(Node node, free_node_func free_node) {
+    if (free_node) free_node(node);
+}
+
+
+void list_free(List list) {
+    if (!list) return;
+    if (!list->nodes) return;
+    if (!list->free_node) ;
+
+    Node node = list->nodes;
+
+    while (node->next) {
+        Node temp = node;
+        node = node->next;
+        list->free_node(temp);
+    }
+
+    free(list);
 }
--- list/list.h
+++ list/list.h
@@ -7,22 +7,23 @@ typedef void * datatype;
 typedef struct list *List;
 typedef struct node *Node;
 typedef int (*compare_func)(datatype a, datatype b);
+typedef void (*free_node_func)(Node node);
 
 
-List list_init(void);
+List list_init(compare_func compare, free_node_func free_node);
 int list_insert_head(List list, datatype data);
 int list_insert_tail(List list, datatype data);
 int list_remove_head(List list);
 int list_remove_tail(List list);
 int list_sort(List list);
-Node list_parent(List list, datatype data);
+Node list_parent(List list, Node target);
 Node list_search(List list, datatype target);
 uint64_t list_size(List list);
 void list_free(List list);
 
 // freeing a node that is the head of a sub-list of the whole list 
 // will cause a leak in memory and loss of data
-void list_node_free(Node node);
+void list_node_free(Node node, free_node_func free_node);
 
 
 #endif
